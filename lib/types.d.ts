83bc749091c4aa31968461d6a305f6106669c6cd
quotingg. <112746878+quotingg@users.noreply.github.com>
2024-09-16 19:51:45 -0400

    Meriyah Migration #1

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,5 +1,5 @@
 declare module '@dynamic-pkg/bare-client';
-declare module '@dynamic-pkg/acorn';
+declare module 'meriyah';
 declare module '@dynamic-pkg/astring';
 declare module '@dynamic-pkg/cookie';
 declare module '@dynamic-pkg/mime';
